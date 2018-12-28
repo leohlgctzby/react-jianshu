2389807a8415daeb8fd8e03812b276766e978166
leohlgctzby <18207221@qq.com>
2018-12-28 20:09:36 +0800

    store-immutable

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,39 +1,16 @@
-// const defaultState = {
-//   focused: false
-// };
-
-// export default (state = defaultState, action) => {
-
-//   // if(action.type === 'search_focus'){
-//   //   return {
-//   //     focused: true
-//   //   };
-//   // }
-
-//   if(action.type === 'search_focus'){
-//     return {
-//       focused: true
-//     };
-//   }
-//   console.log(state)
-//   return state;
-// }
 import * as constants from './constants';
+import { fromJS } from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-};
+});
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    };
+    return state.set('focused', true);
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    };
+    return state.set('focused', false);
   }
   return state;
 };
