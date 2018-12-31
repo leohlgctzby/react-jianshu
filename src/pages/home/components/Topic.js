4a1a45c78cfd3f3fc418492255ec5168a712ad89
leohlgctzby <18207221@qq.com>
2018-12-31 21:09:39 +0800

    TopicItem

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { TopicWrapper, TopicItem } from "../style";
 
 class Topic extends Component {
   render() {
     return (
-      <div>Topic</div>
-    )
+      <TopicWrapper>
+        <TopicItem>
+          <img
+            className="topic-pic"
+            src="//upload-images.jianshu.io/upload_images/2023205-9ec2e0fbff7606f7?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240"
+            alt=""
+          />
+          社会热点
+        </TopicItem>
+      </TopicWrapper>
+    );
   }
 }
 
