aee2fba03765b9b071989238140086d30587deea
leohlgctzby <18207221@qq.com>
2018-12-31 17:04:21 +0800

    spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -41,8 +41,8 @@ class Header extends Component {
         >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch onClick={() => {handleChangePage(page % totalPage + 1)}}>
-              <i className="iconfont spin">&#xe851;</i>
+            <SearchInfoSwitch onClick={() => {handleChangePage(page, totalPage, this.spinIcon)}}>
+              <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
               换一批
             </SearchInfoSwitch>
           </SearchInfoTitle>
@@ -119,8 +119,19 @@ const mapDispatchToProps = dispatch => {
     handleMouseLeave() {
       dispatch(actionCreators.mouseLeave());
     },
-    handleChangePage(page) {
-      dispatch(actionCreators.changePage(page));
+    handleChangePage(page, totalPage, spin) {
+      let originAngle = spin.style.transform.replace(/[^0-9]/ig,'')
+      if (originAngle) {
+        originAngle = parseInt(originAngle, 10);
+      }else{
+        originAngle = 0;
+      }
+      spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)';
+      if (page < totalPage ) {
+      dispatch(actionCreators.changePage(page + 1));
+      }else {
+        dispatch(actionCreators.changePage(1));
+      }
     },
   };
 };
