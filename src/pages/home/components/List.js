c516e1d949eb56b12e3a883851d9f06951cff4cb
leohlgctzby <18207221@qq.com>
2019-01-02 19:38:07 +0800

    home跳转到/detail和PureComponent

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { ListItem, ListInfo, LoadMore } from "../style";
 import { connect } from "react-redux";
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-class List extends Component {
+class List extends PureComponent {
   render() {
     const { articleList, getMoreList, page } = this.props;
     return (
       <div>
         {articleList.map((item,index) => (
-          <ListItem key={index}>
+          <Link key={index} to='/detail'>
+          <ListItem >
             <img className="pic" src={item.get("imgUrl")} alt="" />
             <ListInfo>
               <h3 className="title">{item.get("title")}</h3>
               <p className="desc">{item.get("desc")}</p>
             </ListInfo>
           </ListItem>
+          </Link>
         ))}
         <LoadMore onClick={() => getMoreList(page)}>更多文字</LoadMore>
       </div>
