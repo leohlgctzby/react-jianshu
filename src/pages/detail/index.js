59efbb9dfda1b9712e18b0d9e2ec2f1a413036f8
leohlgctzby <18207221@qq.com>
2019-01-02 21:09:57 +0800

    detail里的 title  content 从 store取数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import { DetailWrapper, Header, Content } from './style'
+import React, { Component } from "react";
+import { DetailWrapper, Header, Content } from "./style";
+import { connect } from "react-redux";
 
 class Detail extends Component {
   render() {
+    const { title, content } = this.props;
     return (
       <DetailWrapper>
-        <Header>如何成为技术大牛？</Header>
-        <Content>
-          <img src='//upload-images.jianshu.io/upload_images/14105833-b55d9d5ceb88a0c0' alt=''/>
-          <p><b>说到娱乐圈里的明星们</b>，想必很多小伙伴们也都有自己喜欢的明星吧，其实说到娱乐圈里的明星，他们平常面临的压力也是非常大的，因为付出很多，所以得到的也会比平常人要多一些，娱乐圈里的明星们，他们的薪酬应该也都是比较高的，所以在这里也不乏有很多的明星，在平常的饮食中也都体现了他们的豪气。</p>
-          <p>说到娱乐圈里的明星们，想必很多小伙伴们也都有自己喜欢的明星吧，其实说到娱乐圈里的明星，他们平常面临的压力也是非常大的，因为付出很多，所以得到的也会比平常人要多一些，娱乐圈里的明星们，他们的薪酬应该也都是比较高的，所以在这里也不乏有很多的明星，在平常的饮食中也都体现了他们的豪气。</p>
-          <p>说到娱乐圈里的明星们，想必很多小伙伴们也都有自己喜欢的明星吧，其实说到娱乐圈里的明星，他们平常面临的压力也是非常大的，因为付出很多，所以得到的也会比平常人要多一些，娱乐圈里的明星们，他们的薪酬应该也都是比较高的，所以在这里也不乏有很多的明星，在平常的饮食中也都体现了他们的豪气。</p>
-          <p>说到娱乐圈里的明星们，想必很多小伙伴们也都有自己喜欢的明星吧，其实说到娱乐圈里的明星，他们平常面临的压力也是非常大的，因为付出很多，所以得到的也会比平常人要多一些，娱乐圈里的明星们，他们的薪酬应该也都是比较高的，所以在这里也不乏有很多的明星，在平常的饮食中也都体现了他们的豪气。</p>
-          </Content>
+        <Header>{title}</Header>
+        <Content dangerouslySetInnerHTML={{ __html: content }} />
       </DetailWrapper>
-    )
+    );
   }
 }
 
-export default Detail;
+const mapState = state => ({
+  title: state.getIn(["detail", "title"]),
+  content: state.getIn(["detail", "content"])
+});
+
+export default connect(
+  mapState,
+  null
+)(Detail);
