70df7077d7f81c2108d1ea2c240798933f22838a
leohlgctzby <18207221@qq.com>
2019-01-02 19:54:17 +0800

    Link to 路由跳转

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header";
-import Home from './pages/home';
-import Detail from './pages/detail';
+import Home from "./pages/home";
+import Detail from "./pages/detail";
 import store from "./store";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Header />
-          <BrowserRouter>
-            <div>
-              <Route path="/" exact component={Home} />
-              <Route path="/detail" exact component={Detail} />
-            </div>
-          </BrowserRouter>
-        </div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Home} />
+            <Route path="/detail" exact component={Detail} />
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { CSSTransition } from "react-transition-group";
 import { connect } from "react-redux";
 import { actionCreators } from "./store";
+import { Link } from "react-router-dom";
 import {
   HeaderWrapper,
   Logo,
@@ -19,36 +20,53 @@ import {
 } from "./style";
 
 class Header extends Component {
-
   getListArea() {
-    const { focused, list, page, totalPage, mouseIn, handleMouseEnter, handleMouseLeave, handleChangePage } = this.props;
+    const {
+      focused,
+      list,
+      page,
+      totalPage,
+      mouseIn,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleChangePage
+    } = this.props;
     const newList = list.toJS();
     const pageList = [];
 
-    if(newList.length){
-      for(let i = ( page - 1 ) * 10; i < page * 10; i++ ){
+    if (newList.length) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
         pageList.push(
           <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
-        )
+        );
       }
     }
 
-    if (focused||mouseIn) {
+    if (focused || mouseIn) {
       return (
-        <SearchInfo 
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+        <SearchInfo
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch onClick={() => {handleChangePage(page, totalPage, this.spinIcon)}}>
-              <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
+            <SearchInfoSwitch
+              onClick={() => {
+                handleChangePage(page, totalPage, this.spinIcon);
+              }}
+            >
+              <i
+                ref={icon => {
+                  this.spinIcon = icon;
+                }}
+                className="iconfont spin"
+              >
+                &#xe851;
+              </i>
               换一批
             </SearchInfoSwitch>
           </SearchInfoTitle>
-          <SearchInfoList>
-            {pageList}
-          </SearchInfoList>
+          <SearchInfoList>{pageList}</SearchInfoList>
         </SearchInfo>
       );
     } else {
@@ -56,11 +74,13 @@ class Header extends Component {
     }
   }
 
-  render () {
+  render() {
     const { focused, handleInputFocus, handleInputBlur, list } = this.props;
     return (
       <HeaderWrapper>
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
         <Nav>
           <NavItem className="left  active">首页</NavItem>
           <NavItem className="left">下载APP</NavItem>
@@ -100,14 +120,14 @@ const mapStateToProps = state => {
     list: state.getIn(["header", "list"]),
     page: state.getIn(["header", "page"]),
     totalPage: state.getIn(["header", "totalPage"]),
-    mouseIn: state.getIn(["header", "mouseIn"]),
+    mouseIn: state.getIn(["header", "mouseIn"])
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     handleInputFocus(list) {
-      (list.size === 0) && dispatch(actionCreators.getList());
+      list.size === 0 && dispatch(actionCreators.getList());
       // if (list.size === 0){ dispatch(actionCreators.getList()) }
       dispatch(actionCreators.searchFocus());
     },
@@ -121,19 +141,19 @@ const mapDispatchToProps = dispatch => {
       dispatch(actionCreators.mouseLeave());
     },
     handleChangePage(page, totalPage, spin) {
-      let originAngle = spin.style.transform.replace(/[^0-9]/ig,'')
+      let originAngle = spin.style.transform.replace(/[^0-9]/gi, "");
       if (originAngle) {
         originAngle = parseInt(originAngle, 10);
-      }else{
+      } else {
         originAngle = 0;
       }
-      spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)';
-      if (page < totalPage ) {
-      dispatch(actionCreators.changePage(page + 1));
-      }else {
+      spin.style.transform = "rotate(" + (originAngle + 360) + "deg)";
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      } else {
         dispatch(actionCreators.changePage(1));
       }
-    },
+    }
   };
 };
 
