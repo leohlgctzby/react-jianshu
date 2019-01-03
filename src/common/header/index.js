9c6ca5fa3f3c924db7f47e0e7af6a657794ebbce
leohlgctzby <18207221@qq.com>
2019-01-03 18:43:36 +0800

    点击登录 发送axios请求，成功后修改store数据为true

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -75,7 +75,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list, login } = this.props;
     return (
       <HeaderWrapper>
         <Link to="/">
@@ -84,7 +84,10 @@ class Header extends Component {
         <Nav>
           <NavItem className="left  active">首页</NavItem>
           <NavItem className="left">下载APP</NavItem>
-          <NavItem className="right">登录</NavItem>
+          {
+            login ? <NavItem className="right">退出</NavItem> :
+            <Link to='/login'><NavItem className="right">登录</NavItem></Link>
+          }
           <NavItem className="right">
             <i className="iconfont">&#xe636;</i>
           </NavItem>
@@ -120,7 +123,8 @@ const mapStateToProps = state => {
     list: state.getIn(["header", "list"]),
     page: state.getIn(["header", "page"]),
     totalPage: state.getIn(["header", "totalPage"]),
-    mouseIn: state.getIn(["header", "mouseIn"])
+    mouseIn: state.getIn(["header", "mouseIn"]),
+    login: state.getIn(["login", "login"])
   };
 };
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,29 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { LoginWrapper, LoginBox, Input, Button } from './style';
+import { actionCreators } from './store'
 
 class Login extends PureComponent {
   render() {
     return (
       <LoginWrapper>
         <LoginBox>
-          <Input placeholder='账号'/>
-          <Input placeholder='密码'/>
-          <Button>登录</Button>
+          <Input placeholder='账号' innerRef={(input) => {this.account = input}}/>
+          <Input placeholder='密码' type='password'  innerRef={(input) => {this.password = input}}/>
+          <Button onClick={() => this.props.login(this.account, this.password)}>登录</Button>
         </LoginBox>
       </LoginWrapper>
     )
   }
 }
 
-const mapState = state => ({});
-
-const mapDispatch = dispatch => ({});
+const mapDispatch = dispatch => ({
+  login(accountElem, passwordElem) {
+    dispatch(actionCreators.login(accountElem.value, passwordElem.value))
+  }
+});
 
 export default connect(
-  mapState,
+  null,
   mapDispatch
 )(Login);
